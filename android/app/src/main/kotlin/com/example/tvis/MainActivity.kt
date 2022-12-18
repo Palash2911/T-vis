9c3a1d57afe44e3254f6e97ffa9a6b0c68c2eb1f
Palash Rambhia <palashrambhia2@gmail.com>
2022-12-18 19:31:23 +0530

    Project Initialized

---
+++ android/app/src/main/kotlin/com/example/tvis/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tvis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(_Myapp());
+}
+
+class _Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
