6446b6181db6df6ebc5523432c643ca339fd14b2
Palash Rambhia <palashrambhia2@gmail.com>
2023-02-05 17:00:30 +0530

    UI Changes

--- lib/Widgets/bottomNavBar.dart
+++ lib/Widgets/bottomNavBar.dart
@@ -8,7 +8,6 @@ import 'package:tvis/constants.dart';
 import '../Screens/Profile/ProfileScreen.dart';
 
 class bottomNavBar extends StatefulWidget {
-
   @override
   State<bottomNavBar> createState() => _bottomNavBarState();
 }
@@ -28,7 +27,7 @@ class _bottomNavBarState extends State<bottomNavBar> {
   int _page = 0;
   @override
   Widget build(BuildContext context) {
-    final _icons = <Widget>[
+    final icons = <Widget>[
       Icon(
         Icons.home_rounded,
         size: 30,
@@ -51,7 +50,7 @@ class _bottomNavBarState extends State<bottomNavBar> {
         buttonBackgroundColor: kPrimaryColor,
         color: kPrimaryColor,
         backgroundColor: kSecondaryColor,
-        items: _icons,
+        items: icons,
         animationCurve: Curves.easeInOut,
         animationDuration: const Duration(milliseconds: 300),
         onTap: (index) {
