df38ffa84f68597bdca931382a72f38341cf9a7b
DipHire <diphire111@gmail.com>
2023-02-06 14:01:16 +0530

    91% done (Splash Screen Added)

---
+++ lib/Screens/SpalshScreen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: SizedBox(
+            height: 200.0,
+            child: Lottie.asset('assets/animations/logo.json'),
+          ),
+        ),
+      ),
+    );
+  }
+}
